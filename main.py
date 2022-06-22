65e5924ce1bbef1c0c8ff7c5d758e0f9da4495d0
Brahma Keerthi <brahmakeerthihs008@gmail.com>
2022-06-23 00:09:09 +0530

    Successfully sorted list based on start time

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import csv
-import numpy as np
+
+def Sort(sub_li):
+  
+    # reverse = None (Sorts in Ascending order)
+    # key is set to sort using second element of 
+    # sublist lambda has been used
+    sub_li.sort(key = lambda x: x[6])
+    return sub_li
   
 # Open file     
-with open('data.csv') as file_obj:
+with open('new.csv') as file_obj:
       
     # Create reader object by passing the file 
     # object to reader method
     reader_obj = csv.reader(file_obj)
 
     arr = list(reader_obj)
-print(arr)    
+
+# print(arr) 
+
+print(Sort(arr))
+
+  

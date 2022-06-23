556b74df4674aa5890e91f67a19faacf46ef5fe2
Brahma Keerthi <brahmakeerthihs008@gmail.com>
2022-06-23 07:17:18 +0530

    Sorting based on start time

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import numpy as np
 
 def Sort(sub_li):
   
@@ -15,10 +16,18 @@ with open('new.csv') as file_obj:
     # object to reader method
     reader_obj = csv.reader(file_obj)
 
+    # Removes the header from the data to be sorted
     arr = list(reader_obj)
+    header = arr[0]
+    del arr[0]
+    
+arr = Sort(arr)
 
-# print(arr) 
-
-print(Sort(arr))
+# Converting sorted list back to the csv file
+with open('sorted.csv', 'w') as f:
+    write = csv.writer(f)
+      
+    write.writerow(header) # Writes the header
+    write.writerows(arr)  # Writes the rows
 
   
